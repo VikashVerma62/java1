0acd75f6ffddfd83b89c4f2f174a42a678b03daa
VikashVerma62 <vikashverma626506@gmail.com>
2025-04-28 18:39:52 -0700

    update

---
+++ array1.java
@@ -0,0 +1,10 @@
+ public class array1{
+    public static void main(String args[]){
+        int arr[]=new int []{1,2,3,4,5};
+        System.out.println("hello world");
+        for(int i:arr)
+        {
+            System.out.println(i);
+        }
+    }
+ }
