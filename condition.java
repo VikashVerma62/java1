9c4b1f1301b4132f7200caa3ba8249fc57576104
VikashVerma62 <vikashverma626506@gmail.com>
2025-05-02 12:12:01 -0700

    update

---
+++ condition.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class condition{
+    public static void main(String args[]){
+        Scanner sc=new Scanner (System.in);
+        System.out.println("Enter first number");
+        int x=sc.nextInt();
+        System.out.println("Enter second number");
+        int y=sc.nextInt();
+        if(x>y){
+            System.out.println("First number is Greater");
+        }
+    
+        if(x<y){
+            System.out.println("Second number is Greater");
+        }
+            if(x==y){
+                System.out.println("Both Number are Equal");
+            }
+        
+     
+    }
+}
---
+++ incrementOperater.java
@@ -0,0 +1,10 @@
+ public class incrementOperater{
+ public static void main(String args[]){
+    int x=10;
+    int y=5;
+    ++x;
+    --y;
+    System.out.println("Value of x: "+x);
+    System.out.println("Value of y"+y);
+ }
+ }
