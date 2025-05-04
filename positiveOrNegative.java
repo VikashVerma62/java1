33fc91c0012fe9efa73d2fe4eaedfcdaa20d7b73
VikashVerma62 <vikashverma626506@gmail.com>
2025-05-04 16:50:14 -0700

    update

---
+++ positiveOrNegative.java
@@ -0,0 +1,14 @@
+ import java.util.*;
+ public class positiveOrNegative{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number");
+        int n=sc.nextInt();
+        if(n>0){
+            System.out.println("This is Positive Number");
+        }
+        else{
+            System.out.println("This is Negative Number");
+        }
+    }
+ }
